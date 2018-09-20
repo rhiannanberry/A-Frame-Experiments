cc13fb72b90f9beb886f0c844e0041784f36fca3
Rhiannan Berry <rhiannanb97@gmail.com>
2018-09-20 18:47:13 -0400

    race conditions removed

--- experiments/animation-rig/js/main.js
+++ experiments/animation-rig/js/main.js
@@ -8,39 +8,48 @@ AFRAME.registerComponent('rig-control', {
   schema: {
     default: ''
   },
-  init() {
 
+  init: function () {
     var avatar = document.querySelector('#test');
     var scene = document.querySelector('a-scene');
+    var self = this;
+    var model = avatar.getObject3D('mesh');
 
-    var loader = new THREE.FBXLoader();
-
-    //Can load animation sets from a single fbx, and apply it to a different fbx
-    //Or load a single animation from an fb and apply it to a different fbx
-    loader.load('../models/fbx/samba anims.fbx', function (object) {
+    if (model) {
+      self.loadAnimationsFBX(model, '../models/fbx/samba anims.fbx');
+    } else {
       avatar.addEventListener('model-loaded', function () {
-        var model = avatar.getObject3D('mesh');
-        if (!object.animations) {
-          console.warn('animation fbx does not contain animations!');
-          return;
-        }
+        model = avatar.getObject3D('mesh');
+        self.loadAnimationsFBX(model, '../models/fbx/samba anims.fbx');
+      })
+    }
 
-        if (object.animations.constructor === Array) {
-          model.animations = model.animations.concat(object.animations);
-        } else {
-          model.animations.push(object.animations);
-        }
-        console.log(model);
-        document.addEventListener('keydown', function (e) {
-          if (e.key == '1') {
-            console.log('looks painful');
-            avatar.setAttribute('animation-rig', 'clip: Armature|idle; crossFadeDuration: .3;');
-          } else if (e.key == '2') {
-            avatar.setAttribute('animation-rig', 'clip: Armature|dance; crossFadeDuration: .3;');
-          }
-        });
-      });
+    document.addEventListener('keydown', function (e) {
+      if (e.key == '1') {
+        console.log('idle');
+        avatar.setAttribute('animation-rig', 'clip: Armature|idle; crossFadeDuration: .3;');
+      } else if (e.key == '2') {
+        console.log('dance');
+        avatar.setAttribute('animation-rig', 'clip: Armature|dance; crossFadeDuration: .3;');
+      }
     });
+  },
 
+  loadAnimationsFBX: function (model, filename) {
+    var loader = new THREE.FBXLoader();
+    loader.load(filename, function (object) {
+      if (!object.animations) {
+        console.warn('animation fbx does not contain animations!');
+        return;
+      }
+      if (object.animations.constructor === Array) {
+        model.animations = model.animations.concat(object.animations);
+      } else {
+        model.animations.push(object.animations);
+      }
+      console.log(model);
+    });
   }
+
+
 });
